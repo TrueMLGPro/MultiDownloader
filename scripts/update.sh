a0c5447e19a2fac70f24f703d0e60b1df1bbe498
TrueMLGPro <user@gmail.com>
2021-01-31 13:05:38 +0200

    Fixed TERM vars for tput in installer/update scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REQUIRED_PKG="git"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo "$(tput setaf 2)[1/2] $(tput setaf 4)[i] $(tput sgr0)Checking for $REQUIRED_PKG: $PKG_OK"
+echo "$(tput -T xterm setaf 2)[1/2] $(tput -T xterm setaf 4)[i] $(tput -T xterm sgr0)Checking for $REQUIRED_PKG: $PKG_OK"
 if [ "" = "$PKG_OK" ]; then
   echo "[!] No $REQUIRED_PKG found. Setting up $REQUIRED_PKG..."
   apt --yes install $REQUIRED_PKG
 fi
-echo "$(tput setaf 2)[2/2] $(tput setaf 4)[i] $(tput sgr0)Pulling latest changes from repo..."
+echo "$(tput -T xterm setaf 2)[2/2] $(tput -T xterm setaf 4)[i] $(tput -T xterm sgr0)Pulling latest changes from repo..."
 git pull origin master
-echo "$(tput setaf 2)[i] Done.$(tput sgr0)"
+echo "$(tput -T xterm setaf 2)[i] Done.$(tput -T xterm sgr0)"

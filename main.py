ea107f2c4897e1cf96aa287e2fdafea13d60bb66
TrueMLGPro <user@gmail.com>
2021-01-31 12:37:32 +0200

    Update installer/update scripts and fixed arguments (wip)

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--update', help='Updates MultiDownloader')
-parser.add_argument('-c', '--curl', help='Uses curl for download')
-parser.add_argument('-w', '--wget', help='Uses wget for download')
+parser.add_argument('URL', metavar='url', help='a url to download', nargs='?')
+parser.add_argument('-u', '--update', action='store_true', help='Updates MultiDownloader')
+parser.add_argument('-c', '--curl', action='store_true', help='Uses curl for download')
+parser.add_argument('-w', '--wget', action='store_true', help='Uses wget for download')
 parser.parse_args()
 
 def banner():
 	banner_figlet = pyfiglet.figlet_format("MultiDownloader", font="small")
-	print(banner_figlet)
+	print(banner_figlet + "Made by TrueMLGPro | v1.0")
 
 def menu():
 	print("\n" + "1. Download using curl" + "\n"
@@ -35,6 +36,7 @@ def main():
 		elif (choice == "3"):
 			print("[i] Getting latest updates for MultiDownloader..." + "\n")
 			subprocess.call('sh scripts/update.sh', shell=True)
+			print("\n")
 			menu()
 		elif (choice == "4"):
 			print("[!] Exiting...")
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REQUIRED_PKG="git"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo "[1/2] [i] Checking for $REQUIRED_PKG: $PKG_OK"
+echo "$(tput setaf 2)[1/2] $(tput setaf 4)[i] $(tput sgr0)Checking for $REQUIRED_PKG: $PKG_OK"
 if [ "" = "$PKG_OK" ]; then
   echo "[!] No $REQUIRED_PKG found. Setting up $REQUIRED_PKG..."
   apt --yes install $REQUIRED_PKG
 fi
-echo "[2/2] [i] Pulling latest changes from repo..."
+echo "$(tput setaf 2)[2/2] $(tput setaf 4)[i] $(tput sgr0)Pulling latest changes from repo..."
 git pull origin master
-echo "[i] Done."
+echo "$(tput setaf 2)[i] Done.$(tput sgr0)"
